88c9c892487495861b79c19083ed73cf04f54a9a
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-01-16 08:13:37 -0500

    added comments

--- 1_16 MissingNo In Python/missingo.py
+++ 1_16 MissingNo In Python/missingo.py
@@ -8,18 +8,18 @@ print(numList)
 
 def findMissingNo(brokenList):
     missingNo = 0
-    if brokenList == []:
+    if brokenList == []: #case if the list is empty because the original length was one
         missingNo = 1
         brokenList = [1]
-    elif brokenList[0] != 1:
+    elif brokenList[0] != 1: #case if 1 isn't the first index. this saves time i think
         # print("A") debug msg
         missingNo = 1
         brokenList.insert(0,missingNo)
-    elif len(brokenList) + 1 not in brokenList:
+    elif len(brokenList) + 1 not in brokenList: #the last number in the list should be the length of the original list before it was popped
         # print("B") debug msg
         missingNo = len(brokenList) + 1
         brokenList.append(missingNo)
-    else:
+    else: #every other case where some random number in the middle was removed
         # print("C") debug msg
         for nums in range(len(brokenList)):
             if nums != 0:
@@ -30,5 +30,5 @@ def findMissingNo(brokenList):
     print(f"The missing number was {missingNo}.")
     return brokenList
 
-numList = findMissingNo(numList)
+numList = findMissingNo(numList) #fixes the original list
 print(numList)
