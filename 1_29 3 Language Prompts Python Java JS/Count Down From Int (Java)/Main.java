7cb58a76b29eeb3f2729be3c73d987d37b2ae04a
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-01-29 08:11:41 -0500

    added comments

--- 1_29 3 Language Prompts Python Java JS/Count Down From Int (Java)/Main.java
+++ 1_29 3 Language Prompts Python Java JS/Count Down From Int (Java)/Main.java
@@ -5,9 +5,9 @@ public class Main{
         Scanner intScanner = new Scanner(System.in); //scanner object that will read the int the user types
         System.out.print("Type in an integer and I will count down from it:  ");
         int userInt = 0; //starting integer. this is a safe starting value because you can't count down from 0
-        try {
+        try { //error handling
             userInt = intScanner.nextInt();
-        } catch (Exception e) {
+        } catch (Exception e) { //idk why exception is e and not just Exception like in python but that's the default syntax and maybe e needs to be created as an object of Exception because everything's a class?
             System.out.println("That wasn't an integer how am I supposed to count down from that dude that's really uncool of you.");
         }
         System.out.println();
