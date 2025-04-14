19a51f824f6468d1684d530c3f95b91517fec6f1
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-04-14 09:37:16 -0400

    massive progress

--- 4_14 Animal Kingdom/src/Animal.java
+++ 4_14 Animal Kingdom/src/Animal.java
@@ -1,39 +1,128 @@
 // Base class
 // TODO: Define a class called Animal with:
-// - A protected String field called name
-// - A constructor that takes a name and assigns it
-// - A public void method called speak() that prints a generic sound
-// - Override toString() to return "Animal: " + name
-// - Override equals(Object o) to compare names if o is an Animal
+public class Animal {
+    // - A protected String field called name
+    protected String name;
+    // - A constructor that takes a name and assigns it
+    Animal(String name){
+        this.name = name;
+    }
+    // - A public void method called speak() that prints a generic sound
+    public void speak(){
+        System.out.println("animal noise");
+    }
+    // - Override toString() to return "Animal: " + name
+    @Override
+    public String toString(){
+        return "Animal: " + name;
+    }
+    // - Override equals(Object o) to compare names if o is an Animal
+    @Override
+    public boolean equals(Object animal){
+        if(animal instanceof Animal){
+            return ((Animal) animal).name == this.name;
+        }
+        return false;
+    }
+}
+
 
 // Subclass
 // TODO: Define a class called Mammal that extends Animal with:
-// - A protected boolean field called warmBlooded (set to true)
-// - A protected String field called dietType (e.g., "herbivore")
-// - A protected int field called numLegs (e.g., 4)
-// - A protected String field called sound (e.g., "growl")
-// - A constructor that uses super to set name and sets these fields
-// - Override speak() to print the value of sound prefixed by name
+class Mammal extends Animal{
+    // - A protected boolean field called warmBlooded (set to true)
+    protected boolean warmBlooded = true;
+    // - A protected String field called dietType (e.g., "herbivore")
+    protected String dietType;
+    // - A protected int field called numLegs (e.g., 4)
+    protected int numLegs;
+    // - A protected String field called sound (e.g., "growl")
+    protected String sound;
+    // - A constructor that uses super to set name and sets these fields
+    Mammal(String name, boolean warmBlooded, String dietType, int numLegs, String sound){
+        super(name);
+        this.warmBlooded = warmBlooded;
+    }
+
+    // - Override speak() to print the value of sound prefixed by name
+    @Override
+    public void speak(){
+        System.out.println(this.name + ": " + sound);
+    }
+
+}
+
 
 // Subclasses of Mammal
 // TODO: Define a class Dog that extends Mammal
-// - Constructor takes name and passes name, true, "omnivore", 4, "Woof!" to super
-// - Override speak() to print "<name> says: Woof!"
+class Dog extends Mammal{
+    // - Constructor takes name and passes name, true, "omnivore", 4, "Woof!" to super
+    Dog(String name){
+        super(name, true, "omnivore", 4, "Woof!");
+    }
+    // - Override speak() to print "<name> says: Woof!"
+    @Override
+    public void speak(){
+        System.out.println(this.name + " says: Woof!");
+    }
+}
+
 
 // TODO: Define a class Cat that extends Mammal
-// - Constructor takes name and passes name, true, "carnivore", 4, "Meow!" to super
-// - Override speak() to print "<name> says: Meow!"
+class Cat extends Mammal{
+    // - Constructor takes name and passes name, true, "carnivore", 4, "Meow!" to super
+    Cat(String name){
+        super(name, true, "carnivore", 4, "Meow!");
+    }
+    // - Override speak() to print "<name> says: Meow!"
+    @Override
+    public void speak(){
+        System.out.println(this.name + " says: Meow!");
+    }
+
+}
+
 
 // TODO: Define a final class Human that extends Mammal
-// - Constructor takes name and passes name, true, "omnivore", 2, "Hello!" to super
+final class Human extends Mammal{
+    // - Constructor takes name and passes name, true, "omnivore", 2, "Hello!" to super
+    Human(String name){
+        super(name, true, "omnivore", 2, "Hello!");
+    }
+}
+
 
 // TODO: Define a class Reptile that extends Animal
-// - Add fields: boolean warmBlooded (set to false), int numLegs, String dietType, String sound
-// - Constructor should take all values and use super for name
-// - Override speak() to print the value of sound prefixed by name
+class Reptile extends Animal{
+    // - Add fields: boolean warmBlooded (set to false), int numLegs, String dietType, String sound
+    boolean warmBlooded = false;
+    int numLegs;
+    String dietType;
+    String sound;
+    // - Constructor should take all values and use super for name
+    Reptile(String name, boolean warmBlooded, int numLegs, String dietType, String sound){
+        super(name);
+        this.warmBlooded = warmBlooded;
+        this.numLegs = numLegs;
+        this.dietType = dietType;
+        this.sound = sound;
+    }
+
+    // - Override speak() to print the value of sound prefixed by name
+    @Override
+    public void speak(){
+        System.out.println(this.name + ": " + sound);
+    }
+}
+
 
 // Main class to test
 // TODO: Define class AnimalKingdomTest with a main method
+class AnimalKingdomTest{
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
 // - Create Animal a1 = new Dog("Buddy")
 // - Create Animal a2 = new Cat("Whiskers")
 // - Call speak() on both (demonstrating polymorphism)
